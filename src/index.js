ac503870e9282049e5fd74ac0728fa754f54d51a
Rob Kellen <rob.h.kellen@gmail.com>
2021-03-15 16:25:46 -0700

    set up Apollo in index.js to provide access to graph CMS content.  added files for landing page and post page to pages directory.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+//import Apollo packages to manage state
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+//api key for graphCMS content
+const API = process.env.REACT_APP_GRAPHCMS_KEY;
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: API }),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
